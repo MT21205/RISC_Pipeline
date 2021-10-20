587bda733bb3c9503d3a902dfd48c72dd568022e
Nebula95 <ranjith21205@iiitd.ac.in>
2021-10-20 19:58:39 +0530

    Update MA_RW_reg.v
    Updated the MA_RW register to forward the instruction type to the RW stage.

--- MA_RW_reg.v
+++ MA_RW_reg.v
@@ -24,27 +24,33 @@ module MA_RW_reg(
                 input clk,
                 input Data_In,
                 input Inst_In,
+                input Inst_Type_In,
                 output Data_Out,
-                output Inst_Out
+                output Inst_Out,
+                output Inst_Type_Out
                 );
                 
     wire clk;
     wire[31:0] Data_In;
     wire[31:0] Inst_In;
+    wire[4:0] Inst_Type_In;
     
     reg[31:0] Data_Out;
     reg[31:0] Inst_Out;
+    reg[4:0] Inst_Type_Out;
 
     initial
     begin
         Data_Out <= 32'd0;
         Inst_Out <= 32'd0;
+        Inst_Type_Out <= 5'd0;
     end
     
     always@(posedge clk)
     begin
         Data_Out <= Data_In;
         Inst_Out <= Inst_In;
+        Inst_Type_Out <= Inst_Type_In;
     end
     
 endmodule
