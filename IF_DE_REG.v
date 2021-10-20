d947fc4a8021e410abc13ad37f14992b5d640ded
Nebula95 <ranjith21205@iiitd.ac.in>
2021-10-20 19:57:32 +0530

    Update IF_DE_REG.v
    Implemented the instruction kill logic when a branch has to be taken.

--- IF_DE_REG.v
+++ IF_DE_REG.v
@@ -23,6 +23,7 @@
 module IF_DE_REG(input clk,
                  input Inst_In,
                  output Inst_Out,
+                 input branch_kill_flag,
                  output RS1_Addr_out,
                  output RS2_Addr_out,
                  output RD_Addr_out           
@@ -30,6 +31,7 @@ module IF_DE_REG(input clk,
     
     wire clk;
     wire[31:0] Inst_In;
+    wire branch_kill_flag;
     
     reg[31:0] Inst_Out;
     reg[4:0] RS1_Addr_out;
@@ -46,10 +48,24 @@ module IF_DE_REG(input clk,
     
     always@(posedge clk)
     begin
-        Inst_Out <= Inst_In;
-        RS1_Addr_out <= Inst_In[19:15];
-        RS2_Addr_out <= Inst_In[24:20];
-        RD_Addr_out <= Inst_In[11:7];
+        // If a branch is taken, then the instructions in the fetch and the decode stage should to be killed.
+        // Hence, these content aren't to be transmitte to the next stages.
+        // To achieve this, an xx is being sent to the next stages. 
+        if(branch_kill_flag == 1'd1)
+        begin
+            Inst_Out <= 32'dx;
+            RS1_Addr_out <= 5'dx;
+            RS2_Addr_out <= 5'dx;
+            RD_Addr_out <= 5'dx;
+        end
+        else
+        begin
+            Inst_Out <= Inst_In;
+            RS1_Addr_out <= Inst_In[19:15];
+            RS2_Addr_out <= Inst_In[24:20];
+            RD_Addr_out <= Inst_In[11:7];
+        end
+        
     end
     
     
